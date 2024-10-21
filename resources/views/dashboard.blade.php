3932f19d36f4ca4bc08c406a4a43b2cd187fccb5
Luc Hakkesteegt <99072016@mydavinci.nl>
2024-10-21 11:52:33 +0200

    Made pre design for dashboard view
    Made pre design for dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,12 +7,22 @@
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
+    <header class="header">
+        <div class="header-box logo-box">
+            <img src="{{ asset('images/ASVZ_logo.png') }}" alt="Logo" class="logo">
+        </div>
+        <div class="header-box text-box">
+            <div class="header-text">Dashboard</div>
+        </div>
+        <div class="header-box empty-box"></div> <!-- Empty box to maintain grid structure -->
+    </header>
+
     <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1>Dashboard</h1>
-                <p>Welcome to the dashboard</p>
-            </div>
+        <div class="box left-box">
+            Left Box (1/3)
+        </div>
+        <div class="box right-box">
+            Right Box (2/3)
         </div>
     </div>
 </body>
