baa112f7bf61ae9dcd4165ee091550af85003216
Luc Hakkesteegt <99072016@mydavinci.nl>
2024-10-21 11:22:55 +0200

    Completed Starter Application
    Completed Starter Application

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DashBoard ASVZ</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Dashboard</h1>
+                <p>Welcome to the dashboard</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
